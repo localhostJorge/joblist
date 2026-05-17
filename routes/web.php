8df398b231e906e964254908f97341a2dd3dff01
localhostJorge <akosijoshua187@gmail.com>
2026-05-17 21:48:37 +0800

    home route

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/', function () {
     return view('home');
 });
 Route::get('/jobs', [JobController::class, 'index']);
